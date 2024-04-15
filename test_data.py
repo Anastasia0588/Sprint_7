98248b2579c505c469e2f8c34509156d7bdbdbcb
Anastasiia Drozdovskaia <anastasia0588@gmail.com>
2024-04-15 23:34:57 +0300

    переименован

--- test_data.py
+++
@@ -1,8 +0,0 @@
-# ручки
-LOGIN_COURIER = "https://qa-scooter.praktikum-services.ru/api/v1/courier/login"
-CREATE_COURIER = "https://qa-scooter.praktikum-services.ru/api/v1/courier"
-CREATE_ORDER = "https://qa-scooter.praktikum-services.ru/api/v1/orders"
-ORDER_LIST = "https://qa-scooter.praktikum-services.ru/api/v1/orders"
-DELETE_COURIER = "https://qa-scooter.praktikum-services.ru/api/v1/courier/"
-ACCEPT_ORDER = "https://qa-scooter.praktikum-services.ru/api/v1/orders/accept/"
-GET_ORDER_BY_NUMBER = "https://qa-scooter.praktikum-services.ru/api/v1/orders/track"
