38cc501210df2f135111383628931602edd6f778
Anastasiia Drozdovskaia <anastasia0588@gmail.com>
2024-04-15 23:24:25 +0300

    удалены принты, отредактирован урл запроса

--- tests/mandatory_tests/test_create_courier.py
+++ tests/mandatory_tests/test_create_courier.py
@@ -1,35 +1,28 @@
 import pytest
 import requests
 import allure
-import test_data
+import helpers
+from urls import Url, Endpoint
 
 
 class TestCreateCourier:
 
     @allure.title('Создание курьера. Ожидается правильный код и текст ответа')
-    def test_create_courier_success(self, generate_login, generate_password, generate_firstname):
+    def test_create_courier_success(self):
 
-        payload = {
-            "login": generate_login,
-            "password": generate_password,
-            "firstName": generate_firstname
-        }
+        payload = helpers.generate_login_password()
 
-        response = requests.post(test_data.CREATE_COURIER, data=payload)
+        response = requests.post(f"{Url.BASE_URL}{Endpoint.COURIER}", data=payload)
 
         assert response.status_code == 201 and response.json()["ok"] == True
 
     @allure.title('Создание двух одинаковых курьеров')
-    def test_create_existed_courier(self, generate_login, generate_password, generate_firstname):
+    def test_create_existed_courier(self):
 
-        payload = {
-            "login": generate_login,
-            "password": generate_password,
-            "firstName": generate_firstname
-        }
+        payload = payload = helpers.generate_login_password()
 
-        requests.post(test_data.CREATE_COURIER, data=payload)
-        response = requests.post(test_data.CREATE_COURIER, data=payload)
+        requests.post(f"{Url.BASE_URL}{Endpoint.COURIER}", data=payload)
+        response = requests.post(f"{Url.BASE_URL}{Endpoint.COURIER}", data=payload)
 
         assert response.status_code == 409 and response.json()["message"] == "Этот логин уже используется."
         # Баг. Возвращается другой текст в ответе: 'Этот логин уже используется. Попробуйте другой.'
@@ -37,31 +30,23 @@ class TestCreateCourier:
 
     @allure.title('Создание курьера, если одно из обязательных полей пропущено')
     @pytest.mark.parametrize("deleted_field", ["login", "password"])
-    def test_create_courier_without_mandatory_fields(self, generate_login, generate_password, generate_firstname, deleted_field):
+    def test_create_courier_without_mandatory_fields(self, deleted_field):
 
-        payload = {
-            "login": generate_login,
-            "password": generate_password,
-            "firstName": generate_firstname
-        }
+        payload = helpers.generate_login_password()
 
         del payload[deleted_field]
-        response = requests.post(test_data.CREATE_COURIER, data=payload)
+        response = requests.post(f"{Url.BASE_URL}{Endpoint.COURIER}", data=payload)
 
         assert response.status_code == 400 and response.json()["message"] == "Недостаточно данных для создания учетной записи"
 
     @allure.title('Создание курьера, если одно из обязательных полей не заполнено')
     @pytest.mark.parametrize("empty_field", ["login", "password"])
-    def test_create_courier_empty_mandatory_fields(self, generate_login, generate_password, generate_firstname, empty_field):
+    def test_create_courier_empty_mandatory_fields(self, empty_field):
 
-        payload = {
-            "login": generate_login,
-            "password": generate_password,
-            "firstName": generate_firstname
-        }
+        payload = helpers.generate_login_password()
 
         payload[empty_field] = ""
-        response = requests.post(test_data.CREATE_COURIER, data=payload)
+        response = requests.post(f"{Url.BASE_URL}{Endpoint.COURIER}", data=payload)
 
         assert response.status_code == 400 and response.json()["message"] == "Недостаточно данных для создания учетной записи"
 
